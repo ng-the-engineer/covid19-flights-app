488570dd1ae664ee68d406596119306721c4310b
Anthony Ng <anthonyng@melco-resorts.com>
2020-08-21 16:45:12 +0100

    Updates

---
+++ precache-manifest.a1becc920f1f446c86f43e4c2b920a82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46a1b1374f2fe5a395d6f4c5694ff96",
+    "url": "/covid19-flights-app/index.html"
+  },
+  {
+    "revision": "a0653496dd180acdb665",
+    "url": "/covid19-flights-app/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "73914167c057a3f0857c",
+    "url": "/covid19-flights-app/static/css/main.15fd01da.chunk.css"
+  },
+  {
+    "revision": "a0653496dd180acdb665",
+    "url": "/covid19-flights-app/static/js/2.37462a6d.chunk.js"
+  },
+  {
+    "revision": "0ccf9295f6767f64d9073d581a6bf4d2",
+    "url": "/covid19-flights-app/static/js/2.37462a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73914167c057a3f0857c",
+    "url": "/covid19-flights-app/static/js/main.95412f28.chunk.js"
+  },
+  {
+    "revision": "526977aab0e58d849ac3",
+    "url": "/covid19-flights-app/static/js/runtime-main.cb7dab29.js"
+  },
+  {
+    "revision": "d6f2cf5b4f48adced11f3499fa1be491",
+    "url": "/covid19-flights-app/static/media/cabin.d6f2cf5b.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-flights-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
