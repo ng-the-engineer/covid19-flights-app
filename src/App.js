42d1eaee4e57f80bc5f2538601796957f9ff956a
Anthony Ng <anthonyng@melco-resorts.com>
2020-08-23 20:57:44 +0100

    Add media query for different screens

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 import logo from './logo.svg';
 import './css/App.css';
 import './css/grid.css';
+import './css/queries.css';
 
 import Header from './components/Header';
 import HowItWorks from './components/HowItWorks';
@@ -11,6 +13,12 @@ import CasesList from './components/FlightsFilter/CasesList';
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>CovidClean</title>
+        <meta name="description" content="React application" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
       <Header/>
       <HowItWorks/>
       <FlightsFilter/>
