6af4e08b84f2659aeb83aef780c3ebae6415f2fe
SeamusY <seamus.y2016@gmail.com>
2020-09-06 00:14:52 +0800

    Added loading screen and arranged the text to appear correctly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import { Helmet } from "react-helmet";
-import logo from './logo.svg';
+import LoadingOverlay from 'react-loading-overlay';
 import './css/App.css';
 import './css/grid.css';
 import './css/queries.css';
@@ -11,24 +11,48 @@ import FlightsFilter from './components/FlightsFilter/FlightsFilter';
 import CasesList from './components/FlightsFilter/CasesList';
 import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>CovidClean</title>
-        <meta name="title" content="CovidClean" />
-        <meta name="name" content="CovidClean" />
-        <meta name="description" content="CovidClean" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Helmet>
-      <Header/>
-      <HowItWorks/>
-      <FlightsFilter/>
-      <CasesList/>
-      <Footer/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false
+    };
+  };
+
+  searchList() {
+    let newValue = !this.state.loading
+    this.setState({ loading: newValue });
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 5000)  // Remove this, setTime out when the fetch is done via backend.
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>CovidClean</title>
+          <meta name="title" content="CovidClean" />
+          <meta name="name" content="CovidClean" />
+          <meta name="description" content="CovidClean" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Helmet>
+        <Header/>
+        <LoadingOverlay
+          active={this.state.loading}
+          spinner
+          text='Loading your content...'
+          >
+        <HowItWorks/>
+        <FlightsFilter searchList={this.searchList.bind(this)}/>
+        <CasesList/>
+        <Footer/>
+        </LoadingOverlay>
+      </div>
+    );
+  }
 }
 
 export default App;
